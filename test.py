354cd920c9fd0bd381b9aeb53bfe75a90cd5594c
mdsajid10 <mdsajidsalim12.com>
2025-08-31 19:36:01 +0530

    Initial project setup

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+import flask
+import streamlit as st
+import joblib
+import spacy
+import PyPDF2
+from textblob import TextBlob
+
+print("✅ All libraries are installed and working!")
